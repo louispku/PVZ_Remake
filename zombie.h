a5b37865a7081f5aa1bbbb74805b9d5f48bead94
Sylvia <jxuanxie@1226.com>
2022-06-26 23:48:13 +0800

    zombie_second

--- zombie.h
+++ zombie.h
@@ -1,11 +1,15 @@
 #ifndef ZOMBIE_H
 #define ZOMBIE_H
 
+
 #include "basic_zombie.h"
 
+//普通僵尸
 class Zombie : public Basic_Zombie
 {
-
+public:
+    Zombie();
+    void advance(int phase) override;
 };
 
-#endif // ZOMBIE_H
+#endif
