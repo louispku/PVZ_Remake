60873e05fa29e788adcdecfe94211a942f827af7
Sylvia <jxuanxie@1226.com>
2022-06-26 23:47:52 +0800

    zombie_second

---
+++ zombie.cpp
@@ -0,0 +1,44 @@
+#include "basic_zombie.h"
+#include "zombie.h"
+#include <QMovie>
+#include <QPainter>
+
+Zombie::Zombie()
+{
+    hp = 270;
+    atk = 100 * 33 / 1000;
+    speed = 80.0 * 33 / 1000 / 4.7;
+    if (qrand() % 2)
+        setMovie(":resources/images/zombies/Zombie/Zombie.gif");
+    else
+        setMovie(":resources/images/zombies/Zombie/Zombie.gif");
+}
+
+void Zombie::advance(int phase)
+{
+    if (!phase)
+        return;
+    update();
+    if (hp <= 0)
+    {
+        if (state < 2)
+        {
+            state = 2;
+            setMovie(":/resources/images/zombies/Zombie/ZombieDie.gif");
+            setHead(":/resources/images/zombies/Zombie/ZombieHead.gif");
+        }
+        else if (movie->currentFrameNumber() == movie->frameCount() - 1)
+            delete this;
+        return;
+    }
+
+    if (state)
+    {
+        state = 0;
+        if (qrand() % 2)
+            setMovie(":resources/images/zombies/Zombie/Zombie.gif");
+        else
+            setMovie(":resources/images/zombies/Zombie/Zombie.gif");
+    }
+    setX(x() - speed);
+}
